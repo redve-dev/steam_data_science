d14f4559423236b50539190fd84ecb40e5f1a69b
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-07 23:40:03 +0100

    gather data from tf2

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from data_getter import data_getter
 
 def main():
     try:
-        gathered_data = data_getter(input_file='data_files/input.json', LoadFromFile=True, data_path='data_files/data.json')
-        print(gathered_data)
+        gathered_data = data_getter(input_file='data_files/input.json', LoadFromFile=False, data_path='data_files/data.json')
+        # print(gathered_data)
     except:
         exit("something went wrong, check logs file")
 
-    gathered_data.save_to_file('no_permissions_to_write')
+    gathered_data.save_to_file('tf2_data.json')
     
 
 if __name__ == "__main__":
