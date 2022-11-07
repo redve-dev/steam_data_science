949903989e085498db02afb67211fd46ae09715a
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-07 23:05:03 +0100

    add tests

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+from data_getter import data_getter
+
+class tests(unittest.TestCase):
+    def test_invalid_input_file(self):
+        self.assertRaises(OSError, data_getter, input_file='this_path_doesnt_exist.json')
+
+    def test_write_to_file_without_permissions(self):
+        d = data_getter(LoadFromFile=True, data_path='data.json')
+        self.assertRaises(PermissionError, d.save_to_file, path='no_permissions_to_write/data.json')
+
+    def test_reading_invalid_data_file(self):
+        d = data_getter(LoadFromFile=True)
+        self.assertRaises(OSError, d.load_from_file, path='this_file_does_not_exist')
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
