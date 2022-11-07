85c3e247cd2acd7e8230a96a3bf656a06ac2146e
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-07 23:15:43 +0100

    fix wrong file paths

--- tests.py
+++ tests.py
@@ -6,12 +6,11 @@ class tests(unittest.TestCase):
         self.assertRaises(OSError, data_getter, input_file='this_path_doesnt_exist.json')
 
     def test_write_to_file_without_permissions(self):
-        d = data_getter(LoadFromFile=True, data_path='data.json')
+        d = data_getter(input_file='', LoadFromFile=True, data_path='data_files/data.json')
         self.assertRaises(PermissionError, d.save_to_file, path='no_permissions_to_write/data.json')
 
     def test_reading_invalid_data_file(self):
-        d = data_getter(LoadFromFile=True)
-        self.assertRaises(OSError, d.load_from_file, path='this_file_does_not_exist')
+        self.assertRaises(OSError, data_getter,input_file='', LoadFromFile=True, data_path='this_file_does_not_exist')
 
 def main():
     unittest.main()
