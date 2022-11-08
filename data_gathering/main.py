8b6180638dc5fc639a60be285d64daa114f7e33e
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-08 19:42:53 +0100

    improve readability

--- data_gathering/main.py
+++ data_gathering/main.py
@@ -1,13 +1,13 @@
-import data_gathering
+from data_gathering import get_data_from_api, get_data_from_file, save_to_file
 
 def main():
     try:
-        gathered_data = data_gathering.get_data_from_file('data_files/data.json')
-        # gathered_data = data_gathering.get_data_from_api('data_files/input.json')
+        gathered_data = get_data_from_file('data_files/data.json')
+        # gathered_data = get_data_from_api('data_files/input.json')
     except:
         exit("something went wrong, check logs file")
 
-    data_gathering.save_to_file('data.json', data=gathered_data)
+    save_to_file('data.json', data=gathered_data)
     
 
 if __name__ == "__main__":
