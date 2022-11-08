b62f6de37447753b7cffc0f73d44393fca8bfa71
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-08 17:25:44 +0100

    change path to save data

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import data_gathering
 def main():
     try:
         gathered_data = data_gathering.get_data_from_file('data_files/data.json')
+        # gathered_data = data_gathering.get_data_from_api('data_files/input.json')
     except:
         exit("something went wrong, check logs file")
 
-    data_gathering.save_to_file('f2_data.json', data=gathered_data)
+    data_gathering.save_to_file('data.json', data=gathered_data)
     
 
 if __name__ == "__main__":
