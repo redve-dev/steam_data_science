fe09df7dabad6448447820f470384fc671021c1b
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-10 23:24:25 +0100

    resolve mixing file inputs

--- data_processing/main.py
+++ data_processing/main.py
@@ -2,32 +2,30 @@ import json
 import csv
 import matplotlib.pyplot as plt
 
-def weapon_accuracy_plot(weapons, data):
-    del data[-1] # remove taser, as it doesn't have "hits" property
-    accuracy = [val["hits"]*100/val["shots"] for val in data]
-    weapons, accuracy = zip(*sorted(zip(weapons, accuracy)))
-    return accuracy
-
-def weapon_firerate_plot(weapons):
-    with open('csgo_weapons_stats.csv') as f:
-        csvFile = list( csv.DictReader(f) )
-    firing_speed = [val["Fire Rate (RPM)"] for val in csvFile if '(' not in val["Name"]]
-    weapons, firing_speed = zip(*sorted(zip(weapons, firing_speed)))
-    return firing_speed
-
-def main():
+def get_weapon_accuracy():
     with open('processed_data.json') as f:
         data = json.loads(f.read())
+    del data[-1] # remove taser, as it doesn't have "hits" property
+    accuracy =  [(val["name"], val["hits"]*100/val["shots"]) for val in data]
+    return accuracy
 
-    weapons = sorted([val["name"].lower() for val in data])
-    weapons.remove('taser')
-    accuracy = weapon_accuracy_plot(weapons, data)
-    firing_speed = weapon_firerate_plot(weapons)
-    print(len(accuracy))
-    print(len(firing_speed))
-    print(len(weapons))
-    plt.bar(weapons, accuracy, color='blue', alpha=0.5)
-    plt.bar(weapons, firing_speed, color='red', alpha=0.5)
+def get_weapon_firing_speed():
+    with open('csgo_weapons_stats.csv') as f:
+        csvFile = list( csv.DictReader(f) )
+    firing_speed = [ (val["Name"].lower(), val["Fire Rate (RPM)"]) for val in csvFile if '(' not in val["Name"]]
+    return firing_speed
+
+def generate_plot_data():
+        accuracy = get_weapon_accuracy()
+        firing_speed = get_weapon_firing_speed()
+        return [( w, a, f ) for (w, a) in accuracy for (n, f) in firing_speed if w == n]
+
+def plot_values(data):
+    pass
+
+def main():
+    data = generate_plot_data()
+    plot_values(data)
     plt.show()
 
 if __name__ == "__main__":
