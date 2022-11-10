3ab247fc4145e25ee8f9c8b6be04362b697ab30f
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-10 23:52:29 +0100

    add ploting and displaying values

--- data_processing/main.py
+++ data_processing/main.py
@@ -1,18 +1,21 @@
 import json
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
+
+# i plot name of field with it's value, because those values are unsorted, and they mix if i don't do it
 
 def get_weapon_accuracy():
     with open('processed_data.json') as f:
         data = json.loads(f.read())
     del data[-1] # remove taser, as it doesn't have "hits" property
-    accuracy =  [(val["name"], val["hits"]*100/val["shots"]) for val in data]
+    accuracy =  [(val["name"], int(val["hits"])*100/int(val["shots"])) for val in data]
     return accuracy
 
 def get_weapon_firing_speed():
     with open('csgo_weapons_stats.csv') as f:
         csvFile = list( csv.DictReader(f) )
-    firing_speed = [ (val["Name"].lower(), val["Fire Rate (RPM)"]) for val in csvFile if '(' not in val["Name"]]
+    firing_speed = [ (val["Name"].lower(), float(val["Fire Rate (RPM)"])) for val in csvFile if '(' not in val["Name"]]
     return firing_speed
 
 def generate_plot_data():
@@ -21,10 +24,21 @@ def generate_plot_data():
         return [( w, a, f ) for (w, a) in accuracy for (n, f) in firing_speed if w == n]
 
 def plot_values(data):
+    names, accuracy, firing_speed = zip(*data)
+    firing_speed = np.array(firing_speed) * 100/max(firing_speed)
+
+    a = 0.6
+    plt.bar(names, accuracy, color='red', alpha=a, label = 'accuracy %')
+    plt.bar(names, firing_speed, color='blue', alpha=a, label = 'normalized firing speed')
+    plt.legend(loc="upper left")
     pass
+
+def setup_plot():
+    plt.xticks(rotation = 70)
 
 def main():
     data = generate_plot_data()
+    setup_plot()
     plot_values(data)
     plt.show()
 
