0689c75a557821d8524fefe02997804f5fde753b
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-10 20:59:06 +0100

    restructurize the program

--- data_processing/main.py
+++ data_processing/main.py
@@ -2,29 +2,32 @@ import json
 import csv
 import matplotlib.pyplot as plt
 
-def weapon_accuracy_plot(data):
+def weapon_accuracy_plot(weapons, data):
     del data[-1] # remove taser, as it doesn't have "hits" property
-    weapons = [val["name"] for val in data]
     accuracy = [val["hits"]*100/val["shots"] for val in data]
-    plt.title('average accuracy per weapon')
-    _,labels = plt.xticks()
-    plt.setp(labels, rotation=70)
-    plt.bar(weapons, accuracy)
+    weapons, accuracy = zip(*sorted(zip(weapons, accuracy)))
+    return accuracy
 
-def weapon_firerate_plot():
+def weapon_firerate_plot(weapons):
     with open('csgo_weapons_stats.csv') as f:
         csvFile = list( csv.DictReader(f) )
-    weapons = [val["Name"] for val in csvFile if '(' not in val["Name"]]
     firing_speed = [val["Fire Rate (RPM)"] for val in csvFile if '(' not in val["Name"]]
-    plt.bar(weapons, firing_speed, color='pink')
-    
+    weapons, firing_speed = zip(*sorted(zip(weapons, firing_speed)))
+    return firing_speed
 
 def main():
     with open('processed_data.json') as f:
         data = json.loads(f.read())
 
-    weapon_firerate_plot()
-    # weapon_accuracy_plot(data)
+    weapons = sorted([val["name"].lower() for val in data])
+    weapons.remove('taser')
+    accuracy = weapon_accuracy_plot(weapons, data)
+    firing_speed = weapon_firerate_plot(weapons)
+    print(len(accuracy))
+    print(len(firing_speed))
+    print(len(weapons))
+    plt.bar(weapons, accuracy, color='blue', alpha=0.5)
+    plt.bar(weapons, firing_speed, color='red', alpha=0.5)
     plt.show()
 
 if __name__ == "__main__":
